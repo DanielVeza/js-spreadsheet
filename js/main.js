074cafabbe5b26290bd3c07b61da8d0be7e7ea2d
Daniel Veza <veez@hotmail.co.nz>
2021-02-14 19:12:30 +1300

    Small fixes, removing logs and adding comments

--- js/main.js
+++ js/main.js
@@ -32,6 +32,9 @@ const LETTER_MAP = {
   25: 'Z'
 };
 
+/**
+ * Draw the table wrapper.
+ */
 const createTable = () => {
   const table = document.createElement('table');
   const tableHeader = document.createElement('thead');
@@ -40,6 +43,9 @@ const createTable = () => {
   table.appendChild(tableHeader);
   tableHeader.appendChild(tableHeaderRow);
   SPREADSHEET_WRAPPER.appendChild(table);
+  // Create first empty column.
+  const th = document.createElement('th');
+  tableHeader.appendChild(th);
   for (let columnIndex = 0; columnIndex < COLUMN_COUNT; columnIndex++) {
     const th = document.createElement('th');
     th.textContent = convertHeaderNumber(columnIndex);
@@ -50,8 +56,16 @@ const createTable = () => {
   table.appendChild(tableBody);
 }
 
+/**
+ * Draw each row and cell, adding in values where they exist.
+ * @param columnIndex
+ * @return {HTMLTableRowElement}
+ */
 const createRow = (columnIndex) => {
   const tableRow = document.createElement('tr');
+  const td = document.createElement('td');
+  td.textContent = columnIndex;
+  tableRow.appendChild(td);
   for (let rowIndex = 0; rowIndex < ROW_COUNT; rowIndex++) {
     const td = document.createElement('td');
     const input = document.createElement('input');
@@ -59,7 +73,6 @@ const createRow = (columnIndex) => {
     input.setAttribute('data-column-index', columnIndex);
     const cellExistingValue = checkForCellValue(rowIndex, columnIndex);
     if (cellExistingValue.length > 0) {
-      console.log(cellExistingValue);
       // 0 seems a little hacky, but is safe here.
      input.value = cellExistingValue[0].value;
     }
@@ -70,20 +83,48 @@ const createRow = (columnIndex) => {
   return tableRow;
 }
 
+/**
+ * Check if a cell already has a value.
+ *
+ * @param rowIndex
+ * @param columnIndex
+ * @return {*[]}
+ */
 const checkForCellValue = (rowIndex, columnIndex) => {
   return TABLE_VALUES.filter((value) => {
     return value.row === rowIndex && value.column === columnIndex;
   });
 }
 
+/**
+ * Set or Update a table value.
+ * @param element
+ */
 const setTableValues = (element) => {
   const value = element.value;
-  const rowIndex = element.getAttribute('data-row-index');
-  const columnIndex = element.getAttribute('data-column-index');
+  const rowIndex = Number(element.getAttribute('data-row-index'));
+  const columnIndex = Number(element.getAttribute('data-column-index'));
+  const tableValueExists = getTableValueIfExists(rowIndex, columnIndex);
+  if (tableValueExists !== -1) {
+    // Remove the existing value.
+    TABLE_VALUES.splice(tableValueExists, 1);
+  }
   TABLE_VALUES.push({
     row: Number(rowIndex),
     column: Number(columnIndex),
     value: value,
+  });
+}
+
+/**
+ * Check if a value already exists for a table cell.
+ * @param rowIndex
+ * @param columnIndex
+ * @return {number}
+ */
+const getTableValueIfExists = (rowIndex, columnIndex) => {
+  return TABLE_VALUES.findIndex((value) => {
+    return value.row === rowIndex && columnIndex === value.column;
   });
 }
 
@@ -106,12 +147,14 @@ const convertHeaderNumber = (columnIndex) => {
   return `${LETTER_MAP[leadingNumber - 1]}${LETTER_MAP[columnIndex]}`;
 }
 
+// Redraw the table when clicked.
 REDRAW_BUTTON.addEventListener('click', () => {
   const table = SPREADSHEET_WRAPPER.querySelector('table');
   // The remove and redraw takes a few secs, so lets hide the table until the new one is redrawn.
   table.classList.add('hidden');
   table.remove();
   // This is silly. You get no feedback on the refresh without this.
-  setTimeout(() => createTable(), 1000);
+  setTimeout(() => createTable(), 250);
 });
+
 createTable();
